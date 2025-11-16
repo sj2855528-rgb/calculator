4303dd9e96757f5d8188c14ebaae635c4c616f17
sj2855528@gmail.com <sj2855528@gmail.com>
2025-11-17 05:46:52 +0900

    Modify main.py & add icon.png

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # ch 4.2.1 main.py
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox)
+from PyQt5.QtGui import QIcon
 
 class Calculator(QWidget):
     
@@ -10,9 +11,23 @@ class Calculator(QWidget):
         self.initUI()
         
     def initUI(self):
+        self.btn1 = QPushButton('Message', self)
+        self.btn1.clicked.connect(self.activateMessage)
+        
+        vbox = QVBoxLayout()
+        vbox.addStretch()
+        vbox.addWidget(self.btn1)
+        vbox.addStretch(1)
+        
+        self.setLayout(vbox)
+        
         self.setWindowTitle('Calculator')
+        self.setWindowIcon(QIcon('icon.png'))
         self.resize(256, 256)
         self.show()
+
+    def activateMessage(self):
+        QMessageBox.information(self, "information", "Button clicked")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
